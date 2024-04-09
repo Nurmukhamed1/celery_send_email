a0d0fc59f1dfbbd4e00130eb90f4689f78b49405
Nurmukhamed1 <49608444+Nurmukhamed1@users.noreply.github.com>
2024-04-09 12:45:40 +0500

    Update settings.py

--- celery_docker/settings.py
+++ celery_docker/settings.py
@@ -127,8 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # smtp
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nurmaduisenbayev@gmail.com'
-EMAIL_HOST_PASSWORD = 'igel udfr ittm kjuc '
+EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_PASSWORD = 'wd4sd ddsd sdsdf'
 EMAIL_PORT = 587
 
 # redis
